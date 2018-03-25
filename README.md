00d81129ac39a65afa18c3f196e97f1ef5053ab1
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-03-25 20:49:58 +0100

    Tidy up - need to style client side

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 # Bitconnect Carlos Ipsum
+A [Bitconnect Carlos](https://www.youtube.com/watch?v=QKO6IChjojI) Lorem Ipsum generator built with [Node.js](https://nodejs.org/en/). A deployed
+version can be viewed [here](https://secret-tundra-67488.herokuapp.com/).
+
+## Getting Started
+```
+npm install
+```
+
+## Running Locally
+```
+npm start
+```
---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+const port = process.env.PORT || 5000;
+const quotes = require('./quotes.json');
+const quotesInParagraph = 10;
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'))
+});
+
+app.get('/text', (req, res) => {
+  const paragraphs = createParagraphs(req.query.paragraphs);
+  res.send(paragraphs);
+});
+
+function createParagraphs(numberOfParagraphs) {
+  const paragraphs = [];
+  for (let i = 0; i < numberOfParagraphs; i++) {
+    paragraphs.push(createParagraph());
+  }
+  return paragraphs;
+}
+
+function createParagraph() {
+  let paragraph = '';
+  for (let i = 0; i < quotesInParagraph; i++) {
+    paragraph += `${getRandomQuote()} `;
+  }
+  return paragraph.trim();
+}
+
+function getRandomQuote() {
+  return quotes[Math.floor(Math.random() * quotes.length)];
+}
+
+app.listen(port, () => console.log(`Listening on ${port}`));
