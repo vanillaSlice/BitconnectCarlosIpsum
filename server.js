9d8fa65e7d41766adf2d73225aa8e1528d62ec52
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-03-26 19:59:31 +0100

    Added seperate api routes

--- server.js
+++ server.js
@@ -1,26 +1,13 @@
 const express = require('express');
 const app = express();
 const path = require('path');
-
 const port = process.env.PORT || 5000;
 const quotes = require('./quotes.json');
 const quotesInParagraph = 10;
 
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'index.html'))
-});
-
-app.get('/text', (req, res) => {
-  const paragraphs = createParagraphs(req.query.paragraphs);
-  res.send(paragraphs);
-});
-
-app.get('/random', (req, res) => {
-  res.send({
-    text: getRandomQuote(),
-    response_type: 'in_channel'
-  });
-});
+/*
+ * Helper functions
+ */
 
 function createParagraphs(numberOfParagraphs) {
   const paragraphs = [];
@@ -41,5 +28,40 @@ function createParagraph() {
 function getRandomQuote() {
   return quotes[Math.floor(Math.random() * quotes.length)];
 }
+
+/*
+ * Routes
+ */
+
+app.get('/', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'index.html'));
+});
+
+app.get('/api/quotes', (req, res) => {
+  res.send({
+    quotes
+  });
+});
+
+app.get('/api/quotes/random', (req, res) => {
+  res.send({
+    quote: getRandomQuote()
+  });
+});
+
+app.get('/api/slack', (req, res) => {
+  res.send({
+    text: getRandomQuote(),
+    response_type: 'in_channel'
+  });
+});
+
+// TODO
+// add p tags and header tags
+// limit number of paragraphs
+app.get('/text', (req, res) => {
+  const paragraphs = createParagraphs(req.query.paragraphs);
+  res.send(paragraphs);
+});
 
 app.listen(port, () => console.log(`Listening on ${port}`));
