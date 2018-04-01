e172f2fb5f93b8e1399804ec0dbecad340cbb151
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-04-01 14:37:49 +0100

    Separated files and completed back end testing

--- server/src/server.js
+++ server/src/server.js
@@ -1,65 +1,30 @@
 const express = require('express');
 const path = require('path');
-const { query, validationResult } = require('express-validator/check');
-const headings = require('./headings.json');
-const quotes = require('./quotes.json');
+const {
+  hTagLevelValidator,
+  includePTagsValidator,
+  paragraphsValidator,
+  includeHeadingsValidator,
+  minQuotesAndMaxQuotesValidator,
+  minQuotesLessThanMaxQuotesValidator,
+  maxQuotesGreaterThanMinQuotesValidator,
+  errorHandler,
+} = require('./validators');
+const {
+  getHeadings,
+  getRandomHeading,
+  getQuotes,
+  getRandomQuote,
+  getText,
+} = require('./helpers');
 
 const app = express();
 const port = process.env.PORT || 3001;
 
 /*
- * Validators
- */
-
-const hTagLevelValidator = query('hTagLevel')
-  .isInt({ min: 1, max: 6 })
-  .withMessage('must be between 1 and 6')
-  .toInt()
-  .optional();
-
-const includePTagsValidator = booleanValidator('includePTags');
-
-const includeHeadingsValidator = booleanValidator('includeHeadings');
-
-function booleanValidator(field) {
-  return query(field)
-    .isBoolean()
-    .withMessage('must be a boolean')
-    .toBoolean(true)
-    .optional();
-}
-
-const paragraphsValidator = query('paragraphs')
-  .isInt({ min: 1, max: 100 })
-  .withMessage('must be between 1 and 100')
-  .toInt();
-
-const minQuotesAndMaxQuotesValidator = query(['minQuotes', 'maxQuotes'])
-  .isInt({ min: 1, max: 20 })
-  .withMessage('must be between 1 and 20')
-  .toInt();
-
-const minQuotesLessThanMaxQuotesValidator = query('minQuotes')
-  .custom((value, { req }) => {
-    if (value > req.query.maxQuotes) {
-      throw new Error('minQuotes must be less than maxQuotes');
-    }
-    return value;
-  });
-
-const maxQuotesGreaterThanMinQuotesValidator = query('maxQuotes')
-  .custom((value, { req }) => {
-    if (value < req.query.minQuotes) {
-      throw new Error('maxQuotes must be greater than minQuotes');
-    }
-    return value;
-  });
-
-/*
  * Using static client files only in production because client runs
  * on its own server in development.
  */
-
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join('..', 'client', 'build')));
 
@@ -68,44 +33,20 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-/*
- * Routes
- */
-
-app.get('/api/headings', [hTagLevelValidator], (req, res) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    res.send({ headings: getHeadings(req.query.hTagLevel) });
-  } else {
-    res.status(422).send({ errors: errors.mapped() });
-  }
+app.get('/api/headings', [hTagLevelValidator, errorHandler], (req, res) => {
+  res.send({ headings: getHeadings(req.query.hTagLevel) });
 });
 
-app.get('/api/headings/random', [hTagLevelValidator], (req, res) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    res.send({ heading: getRandomHeading(req.query.hTagLevel) });
-  } else {
-    res.status(422).send({ errors: errors.mapped() });
-  }
+app.get('/api/headings/random', [hTagLevelValidator, errorHandler], (req, res) => {
+  res.send({ heading: getRandomHeading(req.query.hTagLevel) });
 });
 
-app.get('/api/quotes', [includePTagsValidator], (req, res) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    res.send({ quotes: getQuotes(req.query.includePTags) });
-  } else {
-    res.status(422).send({ errors: errors.mapped() });
-  }
+app.get('/api/quotes', [includePTagsValidator, errorHandler], (req, res) => {
+  res.send({ quotes: getQuotes(req.query.includePTags) });
 });
 
-app.get('/api/quotes/random', [includePTagsValidator], (req, res) => {
-  const errors = validationResult(req);
-  if (errors.isEmpty()) {
-    res.send({ quote: getRandomQuote(req.query.includePTags) });
-  } else {
-    res.status(422).send({ errors: errors.mapped() });
-  }
+app.get('/api/quotes/random', [includePTagsValidator, errorHandler], (req, res) => {
+  res.send({ quote: getRandomQuote(req.query.includePTags) });
 });
 
 app.get(
@@ -118,106 +59,10 @@ app.get(
     minQuotesAndMaxQuotesValidator,
     minQuotesLessThanMaxQuotesValidator,
     maxQuotesGreaterThanMinQuotesValidator,
+    errorHandler,
   ], (req, res) => {
-    const errors = validationResult(req);
-    if (errors.isEmpty()) {
-      res.send({ text: getText(req.query) });
-    } else {
-      res.status(422).send({ errors: errors.mapped() });
-    }
+    res.send({ text: getText(req.query) });
   },
 );
-
-/*
- * Helper functions
- */
-
-function getRandomInt(min, max) {
-  return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
-function getRandomArrayElement(array) {
-  return array[getRandomInt(0, array.length - 1)];
-}
-
-function addTag(tag, content) {
-  return `<${tag}>${content}</${tag}>`;
-}
-
-function getHeadings(hTagLevel) {
-  return hTagLevel ? addHTags(hTagLevel, headings) : headings;
-}
-
-function getRandomHeading(hTagLevel) {
-  const heading = getRandomArrayElement(headings);
-  return hTagLevel ? addHTag(hTagLevel, heading) : heading;
-}
-
-function addHTags(hTagLevel, headings) {
-  return headings.map(heading => addHTag(hTagLevel, heading));
-}
-
-function addHTag(hTagLevel, heading) {
-  return addTag(`h${hTagLevel}`, heading);
-}
-
-function getQuotes(includePTags) {
-  return includePTags ? addPTags(quotes) : quotes;
-}
-
-function getRandomQuote(includePTags) {
-  const quote = getRandomArrayElement(quotes);
-  return includePTags ? addPTag(quote) : quote;
-}
-
-function addPTags(paragraphs) {
-  return paragraphs.map(paragraph => addPTag(paragraph));
-}
-
-function addPTag(paragraph) {
-  return addTag('p', paragraph);
-}
-
-function getText(options) {
-  const text = [];
-  for (let i = 0; i < options.paragraphs; i += 1) {
-    text.push(getTextEntry(options));
-  }
-  return text;
-}
-
-function getTextEntry(options) {
-  const entry = {};
-
-  if (options.includeHeadings) {
-    entry.heading = getRandomHeading(options.hTagLevel);
-  }
-
-  entry.paragraph = getParagraph(options);
-
-  return entry;
-}
-
-function getParagraph(options) {
-  let paragraph = '';
-
-  const quotesInParagraph = getRandomInt(options.minQuotes, options.maxQuotes);
-
-  for (let i = 0; i < quotesInParagraph; i += 1) {
-    paragraph += `${getRandomArrayElement(quotes)} `;
-  }
-
-  paragraph = paragraph.trim();
-
-  if (options.includePTags) {
-    paragraph = addPTag(paragraph);
-  }
-
-  return paragraph;
-}
-
-/*
- * Start the server
- */
 
 module.exports = app.listen(port, () => console.log(`Listening on ${port}`));
--- server/src/server.test.js
+++ server/src/server.test.js
@@ -3,29 +3,6 @@ const server = require('./server');
 const headings = require('./headings.json');
 const quotes = require('./quotes.json');
 
-/*
- * Test helpers
- */
-
-function expectInvalidHTagLevelsToThrowErrors(baseUrl) {
-  expectInvalidHTagLevelToThrowError(baseUrl, 0);
-  expectInvalidHTagLevelToThrowError(baseUrl, 7);
-  expectInvalidHTagLevelToThrowError(baseUrl, '');
-  expectInvalidHTagLevelToThrowError(baseUrl, 'bitconneeeeect');
-}
-
-function expectInvalidHTagLevelToThrowError(baseUrl, hTagLevel) {
-  it(`?hTagLevel=${hTagLevel} returns an error`, (done) => {
-    request(server)
-      .get(`${baseUrl}?hTagLevel=${hTagLevel}`)
-      .expect(422, done);
-  });
-}
-
-/*
- * Tests
- */
-
 describe('GET /api/headings', () => {
   const baseUrl = '/api/headings';
 
@@ -44,7 +21,13 @@ describe('GET /api/headings', () => {
     });
   }
 
-  expectInvalidHTagLevelsToThrowErrors(baseUrl);
+  [0, 7, '', NaN, 'bitconneeeeect'].forEach((hTagLevel) => {
+    it(`?hTagLevel=${hTagLevel} returns an error`, (done) => {
+      request(server)
+        .get(`${baseUrl}?hTagLevel=${hTagLevel}`)
+        .expect(422, done);
+    });
+  });
 });
 
 describe('GET /api/headings/random', () => {
@@ -70,36 +53,34 @@ describe('GET /api/headings/random', () => {
     ));
   }
 
-  expectInvalidHTagLevelsToThrowErrors(baseUrl);
+  [0, 7, '', NaN, 'bitconneeeeect'].forEach((hTagLevel) => {
+    it(`?hTagLevel=${hTagLevel} returns an error`, (done) => {
+      request(server)
+        .get(`${baseUrl}?hTagLevel=${hTagLevel}`)
+        .expect(422, done);
+    });
+  });
 });
 
 describe('GET /api/quotes', () => {
   const baseUrl = '/api/quotes';
 
-  function expectReturnsArrayOfAllQuotes(message, url) {
-    it(message, (done) => {
-      request(server)
-        .get(url)
-        .expect(200, { quotes }, done);
-    });
-  }
+  it('returns array of all quotes', (done) => {
+    request(server)
+      .get(baseUrl)
+      .expect(200, { quotes }, done);
+  });
 
-  expectReturnsArrayOfAllQuotes('returns array of all quotes', baseUrl);
-
-  expectReturnsArrayOfAllQuotes(
-    '?includePTags=false returns array of all quotes without surrounding <p> tags',
-    `${baseUrl}?includePTags=false`,
-  );
-
-  expectReturnsArrayOfAllQuotes(
-    '?includePTags= returns array of all quotes without surrounding <p> tags',
-    `${baseUrl}?includePTags=`,
-  );
-
-  expectReturnsArrayOfAllQuotes(
-    '?includePTags=bitconneccccct returns array of all quotes without surrounding <p> tags',
-    `${baseUrl}?includePTags=bitconneccccct`,
-  );
+  [false, 'TRUE', '', NaN, 'bitconneeeeect'].forEach((includePTags) => {
+    it(
+      `?includePTags=${includePTags} returns array of all quotes without surrounding <p> tags`,
+      (done) => {
+        request(server)
+          .get(`${baseUrl}?includePTags=${includePTags}`)
+          .expect(200, { quotes }, done);
+      },
+    );
+  });
 
   it('?includePTags=true returns array of all quotes with surrounding <p> tags', (done) => {
     const quotesWithTags = quotes.map(quote => `<p>${quote}</p>`);
@@ -112,32 +93,26 @@ describe('GET /api/quotes', () => {
 describe('GET /api/quotes/random', () => {
   const baseUrl = '/api/quotes/random';
 
-  function expectReturnsAQuote(message, url) {
-    it(message, () => (request(server)
-      .get(url)
-      .expect(200)
-      .then((res) => {
-        expect(quotes.includes(res.body.quote)).toBe(true);
-      })
-    ));
-  }
+  it('returns a random quote', () => (request(server)
+    .get(baseUrl)
+    .expect(200)
+    .then((res) => {
+      expect(quotes.includes(res.body.quote)).toBe(true);
+    })
+  ));
 
-  expectReturnsAQuote('returns a random quote', baseUrl);
-
-  expectReturnsAQuote(
-    '?includePTags=false returns a quote without surrounding <p> tags',
-    `${baseUrl}?includePTags=false`,
-  );
-
-  expectReturnsAQuote(
-    '?includePTags= returns a quote without surrounding <p> tags',
-    `${baseUrl}?includePTags=`,
-  );
-
-  expectReturnsAQuote(
-    '?includePTags=bitconneccccct returns a quote without surrounding <p> tags',
-    `${baseUrl}?includePTags=bitconneccccct`,
-  );
+  [false, 'TRUE', '', NaN, 'bitconneeeeect'].forEach((includePTags) => {
+    it(
+      `?includePTags=${includePTags} returns a quote without surrounding <p> tags`,
+      () => (request(server)
+        .get(baseUrl)
+        .expect(200)
+        .then((res) => {
+          expect(quotes.includes(res.body.quote)).toBe(true);
+        })
+      ),
+    );
+  });
 
   it('?includePTags=true returns a quote with surrounding <p> tags', () => (request(server)
     .get(`${baseUrl}?includePTags=true`)
@@ -153,20 +128,135 @@ describe('GET /api/quotes/random', () => {
 describe('GET /api/text', () => {
   const baseUrl = '/api/text';
 
-  // include headings/excludes headings
-  // surrounds headings with h tags
-  // validate minQuote and maxQuote
-  // validate paragraphs
+  it('?includeHeadings=true returns text entries with headings', () => (request(server)
+    .get(`${baseUrl}?paragraphs=5&minQuotes=1&maxQuotes=5&includeHeadings=true`)
+    .expect(200)
+    .then((res) => {
+      const { text } = res.body;
+      text.forEach((textEntry) => {
+        expect(headings.includes(textEntry.heading)).toBe(true);
+      });
+    })
+  ));
 
-  expectInvalidHTagLevelsToThrowErrors(baseUrl);
+  [false, 'TRUE', '', NaN, 'bitconneeeeect'].forEach((includeHeadings) => {
+    it(
+      `?includeHeadings=${includeHeadings} returns text entries without headings`,
+      () => (
+        request(server)
+          .get(`${baseUrl}?paragraphs=5&minQuotes=1&maxQuotes=5&includeHeadings=${includeHeadings}`)
+          .then((res) => {
+            const { text } = res.body;
+            text.forEach((textEntry) => {
+              expect(textEntry.heading).toBe(undefined);
+            });
+          })
+      ),
+    );
+  });
 
-  // function expectInvalidParagraphsToThrowError(message, url) {
-  //   it(message, (done) => {
-  //     request(server)
-  //       .get(url)
-  //       .expect(422, done);
-  //   });
-  // }
+  for (let i = 1; i <= 6; i += 1) {
+    it(`?hTagLevel=${i} returns text entries with headings with surrounding <h${i}> tags`, () => (request(server)
+      .get(`${baseUrl}?paragraphs=5&minQuotes=1&maxQuotes=5&includeHeadings=true&hTagLevel=${i}`)
+      .expect(200)
+      .then((res) => {
+        const { text } = res.body;
+        text.forEach((textEntry) => {
+          const { heading } = textEntry;
+          expect(heading.startsWith(`<h${i}>`)).toBe(true);
+          expect(heading.endsWith(`</h${i}>`)).toBe(true);
+        });
+      })
+    ));
+  }
+
+  [0, 7, '', NaN, 'bitconneeeeect'].forEach((hTagLevel) => {
+    it(`?hTagLevel=${hTagLevel} returns an error`, (done) => {
+      request(server)
+        .get(`${baseUrl}?paragraphs=5&minQuotes=1&maxQuotes=5&includeHeadings=true&hTagLevel=${hTagLevel}`)
+        .expect(422, done);
+    });
+  });
+
+  it('?includePTags=true returns text entries with paragraphs with surrounding <p> tags', () => (request(server)
+    .get(`${baseUrl}?paragraphs=5&minQuotes=1&maxQuotes=5&includePTags=true`)
+    .expect(200)
+    .then((res) => {
+      const { text } = res.body;
+      text.forEach((textEntry) => {
+        const { paragraph } = textEntry;
+        expect(paragraph.startsWith('<p>')).toBe(true);
+        expect(paragraph.endsWith('</p>')).toBe(true);
+      });
+    })
+  ));
+
+  [false, 'TRUE', '', NaN, 'bitconneeeeect'].forEach((includePTags) => {
+    it(
+      `?includePTags=${includePTags} returns text entries with paragraphs without surrounding <p> tags`,
+      () => (
+        request(server)
+          .get(`${baseUrl}?paragraphs=5&minQuotes=1&maxQuotes=5&includePTags=${includePTags}`)
+          .then((res) => {
+            const { text } = res.body;
+            text.forEach((textEntry) => {
+              const { paragraph } = textEntry;
+              expect(paragraph.startsWith('<p>')).toBe(false);
+              expect(paragraph.endsWith('</p>')).toBe(false);
+            });
+          })
+      ),
+    );
+  });
+
+  [0, 101, '', NaN, 'bitconneeeeect'].forEach((paragraphs) => {
+    it(`?paragraphs=${paragraphs} returns an error`, (done) => {
+      request(server)
+        .get(`${baseUrl}?minQuotes=1&maxQuotes=5&includeHeadings=true&paragraphs=${paragraphs}`)
+        .expect(422, done);
+    });
+  });
+
+  [1, 25, 50, 75, 100].forEach((paragraphs) => {
+    it(
+      `?paragraphs=${paragraphs} returns text entries with ${paragraphs} paragraphs`,
+      () => (
+        request(server)
+          .get(`${baseUrl}?minQuotes=1&maxQuotes=5&paragraphs=${paragraphs}`)
+          .then((res) => {
+            expect(res.body.text.length).toBe(paragraphs);
+          })
+      ),
+    );
+  });
+
+  [0, 21, '', NaN, 'bitconneeeeect'].forEach((minQuotes) => {
+    it(`?minQuotes=${minQuotes} returns an error`, (done) => {
+      request(server)
+        .get(`${baseUrl}?paragraphs=1&maxQuotes=5&includeHeadings=true&minQuotes=${minQuotes}`)
+        .expect(422, done);
+    });
+  });
+
+  [0, 21, '', NaN, 'bitconneeeeect'].forEach((maxQuotes) => {
+    it(`?maxQuotes=${maxQuotes} returns an error`, (done) => {
+      request(server)
+        .get(`${baseUrl}?paragraphs=1&minQuotes=1&includeHeadings=true&maxQuotes=${maxQuotes}`)
+        .expect(422, done);
+    });
+  });
+
+  it('?minQuotes=5&maxQuotes=1 returns error', (done) => {
+    request(server)
+      .get(`${baseUrl}?paragraphs=1&minQuotes=5&maxQuotes=1`)
+      .expect(422, done);
+  });
+
+  it('?minQuotes=5&maxQuotes=5 does not return error', (done) => {
+    request(server)
+      .get(`${baseUrl}?paragraphs=1&minQuotes=5&maxQuotes=5`)
+      .expect(200, done);
+  });
 });
 
 afterAll(() => {
