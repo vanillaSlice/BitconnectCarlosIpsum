1349423e2154b6e5c3429f19b2e24730c96075b5
vanillaSlice <mike_lowe_08@hotmail.co.uk>
2018-11-24 22:28:10 +0000

    Redirect to front end for unspecified paths

--- server/src/server.js
+++ server/src/server.js
@@ -32,7 +32,7 @@ app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 if (process.env.NODE_ENV === 'production') {
   app.use(express.static(path.join('..', 'client', 'build')));
 
-  app.get('/', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.join(__dirname, '..', 'client', 'build', 'index.html'));
   });
 }
