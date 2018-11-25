75e726c5964fd69fa81355de8a7b125713a84a59
Mike Lowe <mikelowedev@gmail.com>
2018-11-25 00:54:31 +0000

    Try again redirecting to front end if unspecified path

--- server/src/server.js
+++ server/src/server.js
@@ -62,7 +62,7 @@ app.get(
  * on its own server in development.
  */
 if (process.env.NODE_ENV === 'production') {
-  app.use(express.static(path.join('..', '..', 'client', 'build')));
+  app.use(express.static(path.join(__dirname, '..', '..', 'client', 'build')));
 
   app.use((req, res) => {
     res.sendFile(path.join(__dirname, '..', '..', 'client', 'build', 'index.html'));
